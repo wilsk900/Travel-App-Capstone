ca929bdff9a8472df20a004ea2afe624936527d7
mkroach3-lgtm <mkroach3@gmail.com>
2026-04-06 03:07:40 -0400

    Update mileage_tracker.py

--- mileage_tracker.py
+++ mileage_tracker.py
@@ -4,8 +4,8 @@ def mileage():
     st.header("Mileage Tracker")
     
     #get speed and travel time from the user
-    speed = st.number_input('What is the average speed of the vehicle (mph)?', min_value=0, value=60)
-    time_traveled = st.number_input('How many hours will you be traveling?', min_value=1, value=1)
+    speed = st.number_input('What is the average speed of the vehicle (mph)?', min_value=0)
+    time_traveled = st.number_input('How many hours will you be traveling?', min_value=1)
     
     #display for chart
     st.write('\nHour by Hour Breakdown')
@@ -21,9 +21,14 @@ def mileage():
         breakdown.append({"Hour": hour, "Distance Traveled (miles)": distance})
     
     st.table(breakdown)
+
+    summary = f"Total travel distance at {speed} mph for {time_traveled} hours: {total_dist} miles."
     
+    full_itin_mile = f"
+    Trip Itinerary\n____________\n{final_itinerary}\n\nTransportation Summary\n___________\n{summary}
+    "
     #to save to itinerary file
-    if st.button("Add mileage calculations to intinerary"):
-        summary = f"Total travel distance at {speed} mph for {time_traveled} hours: {total_dist} miles."
-        st.session_state['mileage_summary'] = summary
-        st.write("Summary saved!")
+    if st.download_button("Add mileage calculations to intinerary"):
+        data = full_file_content,
+        file_namee = "full_trip_plan.txt",
+        mime = "text/plain"
